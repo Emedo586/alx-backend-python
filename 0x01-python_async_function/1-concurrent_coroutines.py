61f5be6a3d222295fb3108c1ef88f7730d877e10
Emedo586 <emedopeace99@gmail.com>
2024-04-22 18:00:19 +0000

    Python backend

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,19 @@
 import asyncio
-from typing import List# Replace 'previous_file' with the actual file name
+import random
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n, max_delay):
-    # Spawn wait_random n times with the specified max_delay
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    
-    # Wait for all spawned tasks to finish and collect delays
-    delays = [await task for task in asyncio.as_completed(tasks)]
-    
-    # The delays will already be in ascending order because 'as_completed' returns them as they finish
-    return delays
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """ Waits for ran delay until max_delay, returns list of actual delays """
+    spawn_ls = []
+    delay_ls = []
+    for i in range(n):
+        delayed_task = asyncio.create_task(wait_random(max_delay))
+        delayed_task.add_done_callback(lambda x: delay_ls.append(x.result()))
+        spawn_ls.append(delayed_task)
+
+    for spawn in spawn_ls:
+        await spawn
+
+    return delay_ls
